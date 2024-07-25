405e31ccf667a205746483d9262b3663b9844fce
Andreh,ayyad <andreh,ayad@gmail.com>
2024-07-25 15:17:23 +0300

    It's Time to Duel

---
+++ oop/CCG.js
@@ -0,0 +1,67 @@
+class Cards {
+    constructor(name, cost){
+        this.name = name;
+        this.cost = cost;
+    }
+}
+
+class Unit extends Cards {
+    constructor(name, cost, power, res){
+        super(name, cost);
+        this.power = power;
+        this.res = res;
+    }
+    attack ( target ){
+        if (target instanceof Unit) {
+            target.res -= this.power;
+        }else{
+            throw new Error ("Target must be a unit!")
+        }
+    }
+}
+
+class Effect extends Cards {
+    constructor(name, cost, text, stat, magnitude){
+        super(name,cost);
+        this.text = text;
+        this.stat = stat;
+        this.magnitude = magnitude;
+        
+    }
+
+    play( target ) {
+        if( target instanceof Unit ) {
+            target.res += this.magnitude;
+        } else {
+            throw new Error( "Target must be a effect!" );
+        }
+    }
+}
+
+const red = new Unit("Red Belt Ninja", 3, 3, 4);
+
+const black = new Unit("Black Belt Ninja", 4, 5, 4);
+
+const hard = new Effect("Hard Algorithm", 2, "increase target's resilience by 3", "resilience", +3);
+
+const unhandled = new Effect("Unhandled Promise Rejection", 1, "reduce target's resilience by 2", "resilience", -2);
+
+const pair = new Effect("Pair Programming", 3, "increase target's resilience by 2", "power", +2);
+
+console.log(red);
+console.log(hard);
+hard.play(red);
+console.log(red);
+console.log(hard);
+console.log(black);
+unhandled.play(red);
+console.log(red);
+console.log(unhandled);
+console.log(pair);
+pair.play(red);
+console.log(red);
+console.log(pair);
+red.attack(black);
+black.attack(red);
+console.log(red);
+console.log(black);
