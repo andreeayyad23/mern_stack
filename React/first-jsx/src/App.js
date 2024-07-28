d7261aa900bec8b9ed21d9b9fffbce47f0f01bed
Andreh,ayyad <andreh,ayad@gmail.com>
2024-07-28 11:09:33 +0300

    First-React

---
+++ React/first-jsx/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return(
+    <>
+    <h1>Hello Dojo</h1>
+    <h2>Things i need to do:</h2>
+    <ul>
+        <li>Learn React</li>
+        <li>Climb Mt.Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+    </ul>
+    <button onClick={ ()=> alert("This button has been clicked!") }>Click Me</button>
+
+    </>
+)
+}
+
+export default App;
