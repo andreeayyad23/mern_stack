086a1ee0be351029e7eb66965ac13c03d5a3bb4c
Andreh,ayyad <andreh,ayad@gmail.com>
2024-07-30 11:45:38 +0300

    Hook Forms

---
+++ React/hook-forms/src/App.js
@@ -0,0 +1,12 @@
+import HookForms from './components/userForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HookForms/>
+      </div>
+  );
+}
+
+export default App;
