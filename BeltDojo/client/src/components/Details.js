31caadc5db3c143c44b91ab27e09a81b5cd8636e
Andree Ayyad <165289100+andreeayyad23@users.noreply.github.com>
2024-09-04 14:48:03 +0300

    Update Details.js

--- BeltDojo/client/src/components/Details.js
+++ BeltDojo/client/src/components/Details.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
-import './Navbar.css'; // Import custom CSS
+import './Navbar.css';
 
 const Details = () => {
     const { id } = useParams();
